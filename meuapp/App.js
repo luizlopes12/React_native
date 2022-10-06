44c1896f81c4b15b446655f780435ab21915c5f4
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2022-10-06 17:00:22 -0300

    Update App.js

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,14 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Button,TextInput, Alert } from 'react-native';
 
 
 
 
 export default function App() {
   const [bomdia,setBomdia] = useState('Bomdia')
-    const handleBomdia = () =>{
-      setBomdia('aaaaaaaaaaaaaa')
+    const handleBomdia = (e) =>{
+      setBomdia(e.target.value)
+    }
+    const showText = () =>{
+        Alert.alert(
+        "Titulo do brabo",
+        `Que isso meu patrão, o bgl é: `,
+        [
+          {
+            text: "sai fora mlk",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel"
+          },
+          { text: "tmj meu patrão", onPress: () => console.log("OK Pressed") }
+        ]
+      );
     }
   let nome = 'Luiz'
   return (
@@ -16,8 +30,9 @@ export default function App() {
       <Text>Uepa</Text>
       <Text>{bomdia}</Text>
       <Jobs name='Luiz'/>
+      <TextInput value={bomdia} onChange={handleBomdia}/>
       <Text style={{fontSize: 30}}>{nome}</Text>
-      <Button title='Simbora' style={{ width: 80, height: 200, backgroundColor: '#dadada'}} onPress={handleBomdia}/>
+      <Button title='Simbora' style={{ width: 80, height: 200, backgroundColor: '#dadada'}} />
     </View>
   );
 }
