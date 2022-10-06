4a0b51f8a020d4e8fb2b5c418cbcb7d974c326b1
Luiz Lopes ᴮᴿ <luizlopes12@outlook.com>
2022-10-06 16:32:25 -0300

    Components e states

--- meuapp/App.js
+++ meuapp/App.js
@@ -1,15 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+
+
 
 export default function App() {
+  const [bomdia,setBomdia] = useState('Bomdia')
+    const handleBomdia = () =>{
+      setBomdia('aaaaaaaaaaaaaa')
+    }
   let nome = 'Luiz'
-  let img = 'https://i.imgur.com/0zLgEMa.png'
   return (
     <View style={{margin: 50}}>
       <Text>Uepa</Text>
-      <Text>Bomdia</Text>
-      <Image source={{uri:img}} style={{width: 300, height: 300}}></Image>
+      <Text>{bomdia}</Text>
+      <Jobs name='Luiz'/>
       <Text style={{fontSize: 30}}>{nome}</Text>
+      <Button title='Simbora' style={{ width: 80, height: 200, backgroundColor: '#dadada'}} onPress={handleBomdia}/>
     </View>
   );
+}
+
+
+let img = 'https://i.imgur.com/0zLgEMa.png'
+
+const Jobs = (props) =>{
+  return(
+    <>
+    <Image source={{uri:img}} style={{width: 300, height: 300}}/>
+    <Text>{props.name}</Text>
+    </>
+  )
 }
